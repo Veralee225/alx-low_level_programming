97757d91e984e27b5d503baff3cda842d9d9cb4a
Veralee225 <Veralee225@gmail.com>
2022-03-16 17:40:42 +0100

    I wrote a function that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Write a program that prints _putchar, followed by a new line.
+ * The program should return: Zero
+ */
+int main (void)
+{
+    char *ch = "_putchar";
+
+    while (*ch)
+    {
+        _putchar(*ch);
+        ch++;
+    }
+    _putchar('\n');
+return (0);
+}
