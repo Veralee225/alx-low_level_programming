394ceed23d4733e68f334ebd70b0f0c51d3f04e1
Veralee225 <Veralee225@gmail.com>
2022-03-17 17:44:14 +0100

    I added code and made chages

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,8 +10,9 @@ int print_sign(int n);
 int _abs(int abs);
 int print_last_digit(int pld);
 void jack_bauer(void);
-void print_times_table(void);
-int add(int a, int b);
+void times_table(void);
+int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 
 #endif /* MAIN_H_ */
