82c26f6f68ba1197a04db20a222df500ff7c8399
Vera Adiele <veralee225@gmail.com>
2022-04-22 02:02:06 +0100

    I made some changes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,31 +1,31 @@
-#include <stdlib.h>
-#include <string.h>
-#include "strlen.c"
 #include "lists.h"
-
 /**
- * add_node - Adds a new node at the beginning of list_t list
- * @new_head: Address of node
- * @str: New node to add
- * Return: Address of new element, NULL if it fails
+ * add_node - add a new node at the beginning of a list
+ * @head: head of the linked list
+ * @str: string in the struct
+ *
+ * Return: the address of the new element
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node_ptr = malloc(sizeof(list_t));
+list_t *newnode;
+int newelem = 0;
 
-	new_head = malloc(sizeof(list_t));
+newnode = malloc(sizeof(list_t));
 
-	if (new_head == NULL)
-	{
-		return (NULL);
-	}
-	else
-	{
-		new_head->str = strdup(str);
-		new_head->len = length;
-		new_head->next = *head;
-		*head = new;
-		return (new_head);
-	}
+if (newnode == NULL)
+{
+return (NULL);
+}
+newnode->str = strdup(str);
+
+while (str[newelem])
+{
+newelem++;
+}
+
+newnode->len = newelem;
+newnode->next = *head;
+*head = newnode;
+return (*head);
 }
