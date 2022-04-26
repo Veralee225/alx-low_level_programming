8db8b09ba4cc892602448e53fb7ebda1924e2dbc
Veralee225 <Veralee225@gmail.com>
2022-04-26 19:23:31 +0100

    I added solution for 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for alx school students.
+ * 
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+    listint_t *new;
+    listint_t hello = {"School", 8, NULL};
+    size_t n;
+
+    head = &hello;
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->str = strdup("Hello");
+    new->len = 8;
+    new->next = head;
+    head = new;
+    n = print_list(head);
+    printf("-> %lu element\n", n);
+
+    printf("\n");
+    free(new->str);
+    new->str = NULL;
+    n = print_list(head);
+    printf("-> %lu elements\n", n);
+
+    free(new);
+    return (0);
+}
