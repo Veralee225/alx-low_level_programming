1a12288726258d74c6af00081d0776e6d7df04c8
Veralee225 <Veralee225@gmail.com>
2022-04-27 00:55:43 +0100

    made chages

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,21 +4,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <strings.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
     int n;
     struct listint_s *next;
 } listint_t;
-
+typedef unsigned long int size_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
