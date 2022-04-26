1e155916a77167f33e63fa8fd3988c4d7d7125f7
Veralee225 <Veralee225@gmail.com>
2022-04-26 20:22:16 +0100

    I added solution for list length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - a function that returns the number of element in a linked listint_t list
+ * @h: pointer to the head node of the linked list
+ * Return: size_t, the number of nodes in a linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+    size_t num_of_nodes = 0;
+
+    while (h)
+    {
+        h = h->next;
+        num_of_nodes++;
+    }
+    return (num_of_nodes);
+}
