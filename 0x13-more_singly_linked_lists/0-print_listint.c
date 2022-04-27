e17d67cba182ca9ed346767018e470bb3531b422
Veralee225 <Veralee225@gmail.com>
2022-04-27 17:29:27 +0100

    fix kernel doc style

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- * print_listint - function that prints all the elements of a listint_t list
- * @h: pointer to linked list
- * Return: number of elements in the linkedlist
- */
+* print_listint - function that prints all the elements of a listint_t list
+* @h: pointer to linked list
+* Return: number of elements in the linkedlist
+*/
 
 size_t print_listint(const listint_t *h)
 {
